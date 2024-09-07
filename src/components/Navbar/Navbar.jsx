b74ce686e9c5b6c2b6aac85f8ba844a4b28e7cf5
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-09-06 20:19:34 -0500

    add navbar structure and styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,13 @@
-import { assets } from "../../assets/assets"
-import "./Navbar.css"
+import { assets } from "../../assets/assets";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <img className="logo" src={assets.logo} alt="" />
-      <img className="profile-image" src={assets.profile_image} alt="" />
+      <img className="profile" src={assets.profile_image} alt="" />
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
