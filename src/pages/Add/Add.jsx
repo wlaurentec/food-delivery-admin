d770ac9b2412581da8d52451b272e0d67c6a1a0e
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-09-07 22:08:16 -0500

    add Add.jsx styles

--- src/pages/Add/Add.jsx
+++ src/pages/Add/Add.jsx
@@ -25,22 +25,24 @@ const Add = () => {
             required
           ></textarea>
         </div>
-        <div className="add-category flex-col">
-          <p>Product category</p>
-          <select name="category">
-            <option value="Salad">Salad</option>
-            <option value="Rolls">Rolls</option>
-            <option value="Desserts">Desserts</option>
-            <option value="Sandwich">Sandwich</option>
-            <option value="Cake">Cake</option>
-            <option value="Pure Veg">Pure Veg</option>
-            <option value="Pasta">Pasta</option>
-            <option value="Noodles">Noodles</option>
-          </select>
-        </div>
-        <div className="add-price flex-col">
-          <p>Product price</p>
-          <input type="number" name="price" placeholder="$20" />
+        <div className="add-category-price">
+          <div className="add-category flex-col">
+            <p>Product category</p>
+            <select name="category">
+              <option value="Salad">Salad</option>
+              <option value="Rolls">Rolls</option>
+              <option value="Desserts">Desserts</option>
+              <option value="Sandwich">Sandwich</option>
+              <option value="Cake">Cake</option>
+              <option value="Pure Veg">Pure Veg</option>
+              <option value="Pasta">Pasta</option>
+              <option value="Noodles">Noodles</option>
+            </select>
+          </div>
+          <div className="add-price flex-col">
+            <p>Product price</p>
+            <input type="number" name="price" placeholder="$20" />
+          </div>
         </div>
         <button type="submit" className="add-btn">
           ADD
