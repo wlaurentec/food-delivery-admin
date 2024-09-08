bcc45d39db2ac0ef52d08505a06acd7056e16912
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-09-07 19:21:14 -0500

    add sidebar jsx and structure

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,11 +1,25 @@
-import "./SideBar.css"
+import { assets } from "../../assets/assets";
+import "./SideBar.css";
 
 const SideBar = () => {
   return (
-    <div>
-      
+    <div className="sidebar">
+      <div className="sidebar-options">
+        <div className="sidebar-option">
+          <img src={assets.add_icon} alt="" />
+          <p>Add Items</p>
+        </div>
+        <div className="sidebar-option">
+          <img src={assets.order_icon} alt="" />
+          <p>List Items</p>
+        </div>
+        <div className="sidebar-option">
+          <img src={assets.order_icon} alt="" />
+          <p>Orders</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SideBar
+export default SideBar;
